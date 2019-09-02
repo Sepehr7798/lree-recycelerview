ffd782b50c78a9d4ec72b22cdd8905826c7dc05f
Sepehr <sepehr.b7798@gmail.com>
2019-09-02 06:50:20 +0430

    LREERecyclerView attr renamed to LREEView

--- lree-view/src/main/java/sepehr/lreerecyclerview/LREERecyclerView.kt
+++ lree-view/src/main/java/sepehr/lreerecyclerview/LREERecyclerView.kt
@@ -40,51 +40,51 @@ class LREERecyclerView @JvmOverloads constructor(
     init {
         context.theme.obtainStyledAttributes(
             attrs,
-            R.styleable.LREERecyclerView,
+            R.styleable.LREEView,
             0, R.style.DefaultStyle
         ).apply {
             try {
                 setVisibleOrGone(
                     errorView,
-                    getBooleanOrThrow(R.styleable.LREERecyclerView_lree_show_error)
+                    getBooleanOrThrow(R.styleable.LREEView_lree_show_error)
                 )
                 setVisibleOrGone(
                     emptyView,
-                    getBooleanOrThrow(R.styleable.LREERecyclerView_lree_show_empty)
+                    getBooleanOrThrow(R.styleable.LREEView_lree_show_empty)
                 )
 
                 loadingView.findViewById<TextView>(R.id.lree_loading_text).run {
-                    text = getStringOrThrow(R.styleable.LREERecyclerView_lree_loading_text)
+                    text = getStringOrThrow(R.styleable.LREEView_lree_loading_text)
                     setVisibleOrGone(
                         this,
-                        getBooleanOrThrow(R.styleable.LREERecyclerView_lree_loading_text_visible)
+                        getBooleanOrThrow(R.styleable.LREEView_lree_loading_text_visible)
                     )
                 }
                 errorView.findViewById<TextView>(R.id.lree_empty_error_text).run {
-                    text = getStringOrThrow(R.styleable.LREERecyclerView_lree_error_text)
+                    text = getStringOrThrow(R.styleable.LREEView_lree_error_text)
                     setVisibleOrGone(
                         this,
-                        getBooleanOrThrow(R.styleable.LREERecyclerView_lree_error_text_visible)
+                        getBooleanOrThrow(R.styleable.LREEView_lree_error_text_visible)
                     )
                 }
                 emptyView.findViewById<TextView>(R.id.lree_empty_error_text).run {
-                    text = getStringOrThrow(R.styleable.LREERecyclerView_lree_empty_text)
+                    text = getStringOrThrow(R.styleable.LREEView_lree_empty_text)
                     setVisibleOrGone(
                         this,
-                        getBooleanOrThrow(R.styleable.LREERecyclerView_lree_empty_text_visible)
+                        getBooleanOrThrow(R.styleable.LREEView_lree_empty_text_visible)
                     )
                 }
 
                 errorView.findViewById<ImageView>(R.id.lree_empty_error_icon)
-                    .setImageDrawable(getDrawableOrThrow(R.styleable.LREERecyclerView_lree_error_icon).apply {
-                        val c = getColor(R.styleable.LREERecyclerView_lree_error_tint, 0)
+                    .setImageDrawable(getDrawableOrThrow(R.styleable.LREEView_lree_error_icon).apply {
+                        val c = getColor(R.styleable.LREEView_lree_error_tint, 0)
                         if (c > 0) {
                             DrawableCompat.setTint(this, c)
                         }
                     })
                 emptyView.findViewById<ImageView>(R.id.lree_empty_error_icon)
-                    .setImageDrawable(getDrawableOrThrow(R.styleable.LREERecyclerView_lree_empty_icon).apply {
-                        val c = getColor(R.styleable.LREERecyclerView_lree_empty_tint, 0)
+                    .setImageDrawable(getDrawableOrThrow(R.styleable.LREEView_lree_empty_icon).apply {
+                        val c = getColor(R.styleable.LREEView_lree_empty_tint, 0)
                         if (c > 0) {
                             DrawableCompat.setTint(this, c)
                         }
@@ -92,29 +92,29 @@ class LREERecyclerView @JvmOverloads constructor(
 
                 setVisibleOrGone(
                     retryButton,
-                    getBooleanOrThrow(R.styleable.LREERecyclerView_lree_retry_enabled)
+                    getBooleanOrThrow(R.styleable.LREEView_lree_retry_enabled)
                 )
-                retryButton.text = getStringOrThrow(R.styleable.LREERecyclerView_lree_retry_text)
+                retryButton.text = getStringOrThrow(R.styleable.LREEView_lree_retry_text)
 
-                val reverseLayout =
-                    getBooleanOrThrow(R.styleable.LREERecyclerView_lree_layout_manager_reverse)
+                val spanCount =
+                    getIntegerOrThrow(R.styleable.LREEView_lree_layout_manager_grid_columns)
                 val orientation =
-                    when (getIntegerOrThrow(R.styleable.LREERecyclerView_lree_layout_manager_orientation)) {
+                    when (getIntegerOrThrow(R.styleable.LREEView_lree_layout_manager_orientation)) {
                         0 -> RecyclerView.VERTICAL
                         1 -> RecyclerView.HORIZONTAL
                         else -> throw IllegalArgumentException("Illegal orientation")
                     }
-                when (getIntegerOrThrow(R.styleable.LREERecyclerView_lree_layout_manager)) {
+                val reverseLayout =
+                    getBooleanOrThrow(R.styleable.LREEView_lree_layout_manager_reverse)
+                when (getIntegerOrThrow(R.styleable.LREEView_lree_layout_manager)) {
                     0 -> {
                         recyclerView.layoutManager = LinearLayoutManager(
                             context, orientation, reverseLayout
                         )
                     }
                     1 -> {
-                        val c =
-                            getIntegerOrThrow(R.styleable.LREERecyclerView_lree_layout_manager_grid_columns)
                         recyclerView.layoutManager =
-                            GridLayoutManager(context, c, orientation, reverseLayout)
+                            GridLayoutManager(context, spanCount, orientation, reverseLayout)
                     }
                 }
             } finally {
